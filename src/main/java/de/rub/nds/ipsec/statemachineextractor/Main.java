1b4675758972518c226ec4312d09e9cffd765ac0
Dennis Felsch <dennis.felsch@ruhr-uni-bochum.de>
2019-03-21 17:33:28 +0100

    Simple UDP sending and receiving example using the Transport module from TLS-Attacker

--- src/main/java/de/rub/nds/ipsec/statemachineextractor/Main.java
+++ src/main/java/de/rub/nds/ipsec/statemachineextractor/Main.java
@@ -8,12 +8,28 @@
  */
 package de.rub.nds.ipsec.statemachineextractor;
 
+import de.rub.nds.tlsattacker.transport.udp.ClientUdpTransportHandler;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Dennis Felsch <dennis.felsch at ruhr-uni-bochum.de>
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        ClientUdpTransportHandler udpTH = new ClientUdpTransportHandler(100, "9.9.9.9", 53);
+        try {
+            udpTH.initialize();
+            byte[] txData = new BigInteger("509c0100000100000000000003777777037275620264650000010001", 16).toByteArray();
+            udpTH.sendData(txData);
+            byte[] rxData = udpTH.fetchData();
+            udpTH.closeConnection();
+            System.out.println(new BigInteger(1, rxData).toString(16));
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
