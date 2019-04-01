ef76a4914eb1f65521fc320b6c09a470ad3abeba
Dennis Felsch <dennis.felsch@ruhr-uni-bochum.de>
2019-04-01 15:44:19 +0200

    Add class HashPayload

---
+++ src/main/java/de/rub/nds/ipsec/statemachineextractor/isakmp/HashPayload.java
@@ -0,0 +1,29 @@
+/**
+ * IPsec-StateMachineExtractor - Extract the state machine of an IKEv1/IKEv2 implementation
+ *
+ * Copyright © 2019 Ruhr University Bochum
+ *
+ * Licensed under Apache License 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+package de.rub.nds.ipsec.statemachineextractor.isakmp;
+
+/**
+ *
+ * @author Dennis Felsch <dennis.felsch at ruhr-uni-bochum.de>
+ */
+public class HashPayload extends SimpleBinaryPayload {
+
+    public HashPayload() {
+        super(PayloadTypeEnum.Hash);
+    }
+
+    public byte[] getHashData() {
+        return getBinaryData();
+    }
+
+    public void setHashData(byte[] hashData) {
+        setBinaryData(hashData);
+    }
+
+}
