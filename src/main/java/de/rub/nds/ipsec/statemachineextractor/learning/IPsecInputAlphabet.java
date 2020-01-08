87793fe830e3ccbc97d38bdac03f02a84528a832
Dennis Felsch <dennis.felsch@ruhr-uni-bochum.de>
2020-01-08 17:26:22 +0100

    Extend input alphabet by many invalid messages

--- src/main/java/de/rub/nds/ipsec/statemachineextractor/learning/IPsecInputAlphabet.java
+++ src/main/java/de/rub/nds/ipsec/statemachineextractor/learning/IPsecInputAlphabet.java
@@ -20,17 +20,39 @@ public class IPsecInputAlphabet extends ArrayAlphabet<String> {
         super((new String[]{
             "RESET",
             "NEW_QM_MSG_ID",
+            
             "v1_MM_PSK-SA",
             "v1_MM_PKE-SA",
+            "v1_MM_KE",
+            "v1_MM_No",
+            "v1_MM_ID",
             "v1_MM_KE-No",
-            "v1_MM_KE-<No>-<ID>",
-            "v1_MM*_ID-HASH",
+            "v1_MM_KE-ID",
+            "v1_MM_ID-KE",
+            "v1_MM_No-KE",
+            "v1_MM_KE-No-ID",
+            "v1_MM_No-ID-KE",
+            "v1_MM_ID-KE-No",
+            "v1_MM*_ID",
             "v1_MM*_HASH",
+            "v1_MM*_ID-HASH",
+            "v1_MM*_HASH-ID",
+            
             "v1_AM_PSK-SA-KE-No-ID",
+            "v1_AM_PSK-SA-KE-No",
+            "v1_AM_PSK-SA-KE-ID",
+            "v1_AM_PSK-SA-KE",
+            "v1_AM_PSK-SA-No-ID",
+            "v1_AM_PSK-SA-No",
+            "v1_AM_PSK-SA-ID",
+            "v1_AM_PSK-SA",
             "v1_AM_HASH",
+            
             "v1_QM*_HASH1-SA-No-IDci-IDcr",
             "v1_QM*_HASH3",
+
             "v1_INFO*_DEL",
+            
             "ESP_IPv4_TCP_SYN_SSH",
         }));
     }
