ec6a8c4b56168de538dc34aa96e6db45978de5c3
Dennis Felsch <dennis.felsch@ruhr-uni-bochum.de>
2020-09-29 21:18:07 +0200

    Refactor IdentificationPayload classes

--- src/main/java/de/rub/nds/ipsec/statemachineextractor/ike/v2/datastructures/IdentificationPayloadInitiator.java
+++ src/main/java/de/rub/nds/ipsec/statemachineextractor/ike/v2/datastructures/IdentificationPayloadInitiator.java
@@ -10,54 +10,24 @@ package de.rub.nds.ipsec.statemachineextractor.ike.v2.datastructures;
 
 import de.rub.nds.ipsec.statemachineextractor.ike.GenericIKEParsingException;
 import de.rub.nds.ipsec.statemachineextractor.ike.IKEPayloadTypeEnum;
-import de.rub.nds.ipsec.statemachineextractor.ike.IDTypeEnum;
-import de.rub.nds.ipsec.statemachineextractor.ike.v2.IKEv2ParsingException;
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.util.Arrays;
 
 /**
  *
  * @author Dennis Felsch <dennis.felsch at ruhr-uni-bochum.de>
  */
-public class IdentificationPayloadInitiator extends IKEv2Payload {
-
-    protected static final int ID_HEADER_LEN = 8;
-    protected static final int RESERVED_LEN = 3;
-
-    private IDTypeEnum idType = IDTypeEnum.RESERVED;
-    private byte[] identificationData = new byte[0];
-    private byte[] IDi;
+public class IdentificationPayloadInitiator extends IdentificationPayload {
 
     public IdentificationPayloadInitiator() {
         super(IKEPayloadTypeEnum.IdentificationInitiator);
     }
 
-    public IDTypeEnum getIdType() {
-        return idType;
-    }
-
-    public void setIdType(IDTypeEnum idType) {
-        this.idType = idType;
-    }
-
-    public byte[] getIdentificationData() {
-        return identificationData.clone();
-    }
-
-    public void setIdentificationData(byte[] identificationData) {
-        this.identificationData = identificationData;
-    }
-
     public void setIDi() {
-        IDi = new byte[identificationData.length + RESERVED_LEN + 1];
-        IDi[0] = idType.getValue();
-        System.arraycopy(identificationData, 0, IDi, RESERVED_LEN + 1, identificationData.length);
+        setIDx();
     }
 
     public byte[] getIDi() {
-        return IDi.clone();
+        return getIDx();
     }
 
     @Override
@@ -65,46 +35,9 @@ public class IdentificationPayloadInitiator extends IKEv2Payload {
         return "IDi";
     }
 
-    @Override
-    public int getLength() {
-        return ID_HEADER_LEN + identificationData.length;
-    }
-
-    @Override
-    public void writeBytes(ByteArrayOutputStream baos) {
-        super.writeBytes(baos);
-        baos.write(idType.getValue());
-        baos.write(new byte[RESERVED_LEN], 0, RESERVED_LEN);
-        baos.write(identificationData, 0, identificationData.length);
-    }
-
     public static IdentificationPayloadInitiator fromStream(ByteArrayInputStream bais) throws GenericIKEParsingException {
         IdentificationPayloadInitiator identificationPayload = new IdentificationPayloadInitiator();
         identificationPayload.fillFromStream(bais);
         return identificationPayload;
     }
-
-    @Override
-    protected void setBody(byte[] body) throws IKEv2ParsingException {
-        this.setIdType(IDTypeEnum.get(body[0]));
-        this.setIdentificationData(Arrays.copyOfRange(body, 4, body.length));
-    }
-
-    @Override
-    protected void fillFromStream(ByteArrayInputStream bais) throws GenericIKEParsingException {
-        int length = this.fillGenericPayloadHeaderFromStream(bais);
-        byte[] buffer = new byte[length - GENERIC_PAYLOAD_HEADER_LEN];
-        int readBytes;
-        try {
-            readBytes = bais.read(buffer);
-        } catch (IOException ex) {
-            throw new IKEv2ParsingException(ex);
-        }
-        if (readBytes < length - GENERIC_PAYLOAD_HEADER_LEN) {
-            throw new IKEv2ParsingException("Input stream ended early after " + readBytes + " bytes (should read " + (length - GENERIC_PAYLOAD_HEADER_LEN) + "bytes)!");
-        }
-        this.setBody(buffer);
-        setIDi();
-    }
-
 }
--- src/main/java/de/rub/nds/ipsec/statemachineextractor/ike/v2/datastructures/IdentificationPayloadResponder.java
+++ src/main/java/de/rub/nds/ipsec/statemachineextractor/ike/v2/datastructures/IdentificationPayloadResponder.java
@@ -10,54 +10,24 @@ package de.rub.nds.ipsec.statemachineextractor.ike.v2.datastructures;
 
 import de.rub.nds.ipsec.statemachineextractor.ike.GenericIKEParsingException;
 import de.rub.nds.ipsec.statemachineextractor.ike.IKEPayloadTypeEnum;
-import de.rub.nds.ipsec.statemachineextractor.ike.IDTypeEnum;
-import de.rub.nds.ipsec.statemachineextractor.ike.v2.IKEv2ParsingException;
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.util.Arrays;
 
 /**
  *
  * @author Dennis Felsch <dennis.felsch at ruhr-uni-bochum.de>
  */
-public class IdentificationPayloadResponder extends IKEv2Payload {
-
-    protected static final int ID_HEADER_LEN = 8;
-    protected static final int RESERVED_LEN = 3;
-
-    private IDTypeEnum idType = IDTypeEnum.RESERVED;
-    private byte[] identificationData = new byte[0];
-    private byte[] IDr;
+public class IdentificationPayloadResponder extends IdentificationPayload {
 
     public IdentificationPayloadResponder() {
         super(IKEPayloadTypeEnum.IdentificationResponder);
     }
 
-    public IDTypeEnum getIdType() {
-        return idType;
-    }
-
-    public void setIdType(IDTypeEnum idType) {
-        this.idType = idType;
-    }
-
-    public byte[] getIdentificationData() {
-        return identificationData.clone();
-    }
-
-    public void setIdentificationData(byte[] identificationData) {
-        this.identificationData = identificationData;
-    }
-
     public void setIDr() {
-        IDr = new byte[identificationData.length + RESERVED_LEN + 1];
-        IDr[0] = idType.getValue();
-        System.arraycopy(identificationData, 0, IDr, RESERVED_LEN + 1, identificationData.length);
+        setIDx();
     }
 
     public byte[] getIDr() {
-        return IDr.clone();
+        return getIDx();
     }
 
     @Override
@@ -65,46 +35,9 @@ public class IdentificationPayloadResponder extends IKEv2Payload {
         return "IDr";
     }
 
-    @Override
-    public int getLength() {
-        return ID_HEADER_LEN + identificationData.length;
-    }
-
-    @Override
-    public void writeBytes(ByteArrayOutputStream baos) {
-        super.writeBytes(baos);
-        baos.write(idType.getValue());
-        baos.write(new byte[RESERVED_LEN], 0, RESERVED_LEN);
-        baos.write(identificationData, 0, identificationData.length);
-    }
-
     public static IdentificationPayloadResponder fromStream(ByteArrayInputStream bais) throws GenericIKEParsingException {
         IdentificationPayloadResponder identificationPayload = new IdentificationPayloadResponder();
         identificationPayload.fillFromStream(bais);
         return identificationPayload;
     }
-
-    @Override
-    protected void setBody(byte[] body) throws IKEv2ParsingException {
-        this.setIdType(IDTypeEnum.get(body[0]));
-        this.setIdentificationData(Arrays.copyOfRange(body, 4, body.length));
-    }
-
-    @Override
-    protected void fillFromStream(ByteArrayInputStream bais) throws GenericIKEParsingException {
-        int length = this.fillGenericPayloadHeaderFromStream(bais);
-        byte[] buffer = new byte[length - GENERIC_PAYLOAD_HEADER_LEN];
-        int readBytes;
-        try {
-            readBytes = bais.read(buffer);
-        } catch (IOException ex) {
-            throw new IKEv2ParsingException(ex);
-        }
-        if (readBytes < length - GENERIC_PAYLOAD_HEADER_LEN) {
-            throw new IKEv2ParsingException("Input stream ended early after " + readBytes + " bytes (should read " + (length - GENERIC_PAYLOAD_HEADER_LEN) + "bytes)!");
-        }
-        this.setBody(buffer);
-        setIDr();
-    }
-
 }
