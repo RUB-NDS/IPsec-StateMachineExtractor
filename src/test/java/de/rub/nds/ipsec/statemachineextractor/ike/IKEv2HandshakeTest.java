db975a0de843efe796daa65af8165c49b4cf7de8
Dennis Felsch <dennis.felsch@ruhr-uni-bochum.de>
2020-10-21 14:51:11 +0200

    Tests for Cisco IOS

--- src/test/java/de/rub/nds/ipsec/statemachineextractor/ike/IKEv2HandshakeTest.java
+++ src/test/java/de/rub/nds/ipsec/statemachineextractor/ike/IKEv2HandshakeTest.java
@@ -27,11 +27,11 @@ public class IKEv2HandshakeTest {
 
     @Test
     @Ignore
-    public void testHandhake() throws Exception {
+    public void testHandhakeStrongswan() throws Exception {
         IKEHandshake handshake = new IKEHandshake(500, InetAddress.getByName("10.0.3.10"), 500);
         SecurityAssociationPayloadv2 sa;
         IKEMessage answer;
-        
+
         IKEv2Message msg = new IKEv2Message();
         msg.setExchangeType(ExchangeTypeEnum.IKE_SA_INIT);
         msg.setInitiatorFlag(true);
@@ -39,11 +39,42 @@ public class IKEv2HandshakeTest {
         msg.setResponseFlag(false);
         sa = SecurityAssociationPayloadFactory.V2_P1_AES_128_CBC_SHA1;
         msg.addPayload(sa);
-        //handshake.adjustCiphersuite(sa);
+        handshake.adjustCiphersuite(sa);
         msg.addPayload(handshake.prepareIKEv2KeyExchangePayload(handshake.getNextv2MessageID()));
         msg.addPayload(handshake.prepareIKEv2NoncePayload(handshake.getNextv2MessageID()));
-//        System.out.println(DatatypeHelper.byteArrayToHexDump(handshake.secrets_v2.getHandshakeSA().getDhKeyPair().getPrivate().getEncoded()));
-//        System.out.println(DatatypeHelper.byteArrayToHexDump(handshake.secrets_v2.getHandshakeSA().getDhKeyPair().getPublic().getEncoded()));
+        answer = handshake.exchangeMessage(msg);
+
+        msg = new IKEv2Message();
+        msg.setExchangeType(ExchangeTypeEnum.IKE_AUTH);
+        msg.setInitiatorFlag(true);
+        msg.setVersionFlag(false);
+        msg.setResponseFlag(false);
+        msg.addPayload(handshake.prepareIKEv2IdentificationInitiator());
+        msg.addPayload(handshake.prepareIKEv2AuthenticationPayload());
+        sa = SecurityAssociationPayloadFactory.V2_P2_AES_128_CBC_SHA1_ESN;
+        msg.addPayload(sa);
+        msg.addPayload(handshake.prepareIKEv2TrafficSelectorPayloadInitiator());
+        msg.addPayload(handshake.prepareIKEv2TrafficSelectorPayloadResponder());
+        answer = handshake.exchangeMessage(msg);
+    }
+
+    @Test
+    @Ignore
+    public void testHandhakeCisco() throws Exception {
+        IKEHandshake handshake = new IKEHandshake(500, InetAddress.getByName("10.0.3.2"), 500);
+        SecurityAssociationPayloadv2 sa;
+        IKEMessage answer;
+
+        IKEv2Message msg = new IKEv2Message();
+        msg.setExchangeType(ExchangeTypeEnum.IKE_SA_INIT);
+        msg.setInitiatorFlag(true);
+        msg.setVersionFlag(false);
+        msg.setResponseFlag(false);
+        sa = SecurityAssociationPayloadFactory.V2_P1_AES_128_CBC_SHA1;
+        msg.addPayload(sa);
+        handshake.adjustCiphersuite(sa);
+        msg.addPayload(handshake.prepareIKEv2KeyExchangePayload(handshake.getNextv2MessageID()));
+        msg.addPayload(handshake.prepareIKEv2NoncePayload(handshake.getNextv2MessageID()));
         answer = handshake.exchangeMessage(msg);
 
         msg = new IKEv2Message();
--- src/test/java/de/rub/nds/ipsec/statemachineextractor/learning/IPsecMessageMapperTest.java
+++ src/test/java/de/rub/nds/ipsec/statemachineextractor/learning/IPsecMessageMapperTest.java
@@ -31,6 +31,153 @@ public class IPsecMessageMapperTest {
 
     @Test
     @Ignore
+    public void testMapInputMapOutputCiscoPSKAM() throws Exception {
+        try {
+            String abstractInput, abstractOutput;
+            ContextExecutableInput<SerializableMessage, IPsecConnection> executableInput;
+            SerializableMessage concreteOutput;
+            IPsecMessageMapper instance = new IPsecMessageMapper();
+            IPsecConnection conn = new IPsecConnection(InetAddress.getByName("10.0.3.2"), 500, 2000);
+
+            abstractInput = "v1_AM_PSK-SA-KE-No-ID";
+            executableInput = instance.mapInput(abstractInput);
+            concreteOutput = executableInput.execute(conn);
+            abstractOutput = instance.mapOutput(concreteOutput);
+            assertEquals("v1_AM_SA-V-V-V-V-KE-ID-No-HASH", abstractOutput);
+
+            abstractInput = "v1_AM_HASH";
+            executableInput = instance.mapInput(abstractInput);
+            concreteOutput = executableInput.execute(conn);
+            abstractOutput = instance.mapOutput(concreteOutput);
+            assertEquals("NO_RESPONSE", abstractOutput);
+
+            abstractInput = "v1_QM*_HASH1-SA-No-IDci-IDcr";
+            executableInput = instance.mapInput(abstractInput);
+            concreteOutput = executableInput.execute(conn);
+            abstractOutput = instance.mapOutput(concreteOutput);
+            assertEquals("v1_QM*_HASH-SA-No-ID-ID-ResponderLifetime", abstractOutput);
+
+            abstractInput = "v1_QM*_HASH3";
+            executableInput = instance.mapInput(abstractInput);
+            concreteOutput = executableInput.execute(conn);
+            abstractOutput = instance.mapOutput(concreteOutput);
+            assertEquals("NO_RESPONSE", abstractOutput);
+
+            abstractInput = "ESP_IPv4_TCP_SYN_SSH";
+            executableInput = instance.mapInput(abstractInput);
+            concreteOutput = executableInput.execute(conn);
+            abstractOutput = instance.mapOutput(concreteOutput);
+            assertEquals("ESP_IPv4_TCP_SYNACK", abstractOutput);
+        } catch (Exception ex) {
+            Logger.getLogger(IPsecMessageMapperTest.class.getName()).log(Level.SEVERE, null, ex);
+            throw ex;
+        }
+    }
+
+    @Test
+    @Ignore
+    public void testMapInputMapOutputCiscoPSKMM() throws Exception {
+        try {
+            String abstractInput, abstractOutput;
+            ContextExecutableInput<SerializableMessage, IPsecConnection> executableInput;
+            SerializableMessage concreteOutput;
+            IPsecMessageMapper instance = new IPsecMessageMapper();
+            IPsecConnection conn = new IPsecConnection(InetAddress.getByName("10.0.3.2"), 500, 2000);
+
+            abstractInput = "v1_MM_PSK-SA";
+            executableInput = instance.mapInput(abstractInput);
+            concreteOutput = executableInput.execute(conn);
+            abstractOutput = instance.mapOutput(concreteOutput);
+            assertEquals("v1_MM_SA", abstractOutput);
+
+            abstractInput = "v1_MM_KE-No";
+            executableInput = instance.mapInput(abstractInput);
+            concreteOutput = executableInput.execute(conn);
+            abstractOutput = instance.mapOutput(concreteOutput);
+            assertEquals("v1_MM_KE-No-V-V-V-V", abstractOutput);
+
+            abstractInput = "v1_MM*_ID-HASH";
+            executableInput = instance.mapInput(abstractInput);
+            concreteOutput = executableInput.execute(conn);
+            abstractOutput = instance.mapOutput(concreteOutput);
+            assertEquals("v1_MM*_ID-HASH", abstractOutput);
+
+            abstractInput = "v1_QM*_HASH1-SA-No-IDci-IDcr";
+            executableInput = instance.mapInput(abstractInput);
+            concreteOutput = executableInput.execute(conn);
+            abstractOutput = instance.mapOutput(concreteOutput);
+            assertEquals("v1_QM*_HASH-SA-No-ID-ID-ResponderLifetime", abstractOutput);
+
+            abstractInput = "v1_QM*_HASH3";
+            executableInput = instance.mapInput(abstractInput);
+            concreteOutput = executableInput.execute(conn);
+            abstractOutput = instance.mapOutput(concreteOutput);
+            assertEquals("NO_RESPONSE", abstractOutput);
+
+            abstractInput = "ESP_IPv4_TCP_SYN_SSH";
+            executableInput = instance.mapInput(abstractInput);
+            concreteOutput = executableInput.execute(conn);
+            abstractOutput = instance.mapOutput(concreteOutput);
+            assertEquals("ESP_IPv4_TCP_SYNACK", abstractOutput);
+        } catch (Exception ex) {
+            Logger.getLogger(IPsecMessageMapperTest.class.getName()).log(Level.SEVERE, null, ex);
+            throw ex;
+        }
+    }
+
+    @Test
+    @Ignore
+    public void testMapInputMapOutputCiscoPKEMM() throws Exception {
+        try {
+            String abstractInput, abstractOutput;
+            ContextExecutableInput<SerializableMessage, IPsecConnection> executableInput;
+            SerializableMessage concreteOutput;
+            IPsecMessageMapper instance = new IPsecMessageMapper();
+            IPsecConnection conn = new IPsecConnection(InetAddress.getByName("10.0.3.2"), 500, 2000);
+
+            abstractInput = "v1_MM_PKE-SA";
+            executableInput = instance.mapInput(abstractInput);
+            concreteOutput = executableInput.execute(conn);
+            abstractOutput = instance.mapOutput(concreteOutput);
+            assertEquals("v1_MM_SA", abstractOutput);
+
+            abstractInput = "v1_MM_KE-<ID>-<No>";
+            executableInput = instance.mapInput(abstractInput);
+            concreteOutput = executableInput.execute(conn);
+            abstractOutput = instance.mapOutput(concreteOutput);
+            assertEquals("v1_MM_KE-<ID>-<No>-V-V-V-V", abstractOutput);
+
+            abstractInput = "v1_MM*_HASH";
+            executableInput = instance.mapInput(abstractInput);
+            concreteOutput = executableInput.execute(conn);
+            abstractOutput = instance.mapOutput(concreteOutput);
+            assertEquals("v1_MM*_HASH", abstractOutput);
+
+            abstractInput = "v1_QM*_HASH1-SA-No-IDci-IDcr";
+            executableInput = instance.mapInput(abstractInput);
+            concreteOutput = executableInput.execute(conn);
+            abstractOutput = instance.mapOutput(concreteOutput);
+            assertEquals("v1_QM*_HASH-SA-No-ID-ID-ResponderLifetime", abstractOutput);
+
+            abstractInput = "v1_QM*_HASH3";
+            executableInput = instance.mapInput(abstractInput);
+            concreteOutput = executableInput.execute(conn);
+            abstractOutput = instance.mapOutput(concreteOutput);
+            assertEquals("NO_RESPONSE", abstractOutput);
+
+            abstractInput = "ESP_IPv4_TCP_SYN_SSH";
+            executableInput = instance.mapInput(abstractInput);
+            concreteOutput = executableInput.execute(conn);
+            abstractOutput = instance.mapOutput(concreteOutput);
+            assertEquals("ESP_IPv4_TCP_SYNACK", abstractOutput);
+        } catch (Exception ex) {
+            Logger.getLogger(IPsecMessageMapperTest.class.getName()).log(Level.SEVERE, null, ex);
+            throw ex;
+        }
+    }
+
+    @Test
+    @Ignore
     public void testMapInputMapOutputIKEv1() throws Exception {
         try {
             String abstractInput, abstractOutput;
@@ -101,13 +248,13 @@ public class IPsecMessageMapperTest {
             concreteOutput = executableInput.execute(conn);
             abstractOutput = instance.mapOutput(concreteOutput);
             assertEquals("v2_SAINIT_SA-KE-No-MultipleAuthSupport", abstractOutput);
-            
+
             abstractInput = "v2_AUTH_PSK-IDi-AUTH-SA-TSi-TSr";
             executableInput = instance.mapInput(abstractInput);
             concreteOutput = executableInput.execute(conn);
             abstractOutput = instance.mapOutput(concreteOutput);
             assertEquals("v2_AUTH_IDr-AUTH-SA-TSi-TSr", abstractOutput);
-            
+
             abstractInput = "ESP_IPv4_TCP_SYN_SSH";
             executableInput = instance.mapInput(abstractInput);
             concreteOutput = executableInput.execute(conn);
